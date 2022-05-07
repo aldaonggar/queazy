1f6c5e07f5ebd7fbd2e4915325d7c6ba170261c5
aldaonggar <nur.aldaonggar@student.kuleuven.be>
2022-05-07 18:48:16 +0200

    asa

--- app/src/main/java/be/kuleuven/queazy/MenuActivity.java
+++ app/src/main/java/be/kuleuven/queazy/MenuActivity.java
@@ -6,16 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MenuActivity extends AppCompatActivity {
     private Button btnLogOut;
     private Button btnAddQuizSuggestion;
+    private ImageButton ibProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         btnLogOut = (Button) findViewById(R.id.btnLogOut);
         btnAddQuizSuggestion = (Button) findViewById(R.id.btnAddQuizSuggestion);
+        ibProfile = (ImageButton) findViewById(R.id.ibProfile);
     }
 
     public void onBtnLogOut_Clicked(View caller){
@@ -25,6 +29,11 @@ public class MenuActivity extends AppCompatActivity {
 
     public void onBtnAddQuizSuggestion_Clicked(View caller){
         Intent intent = new Intent(this, AddQuizNameActivity.class);
+        startActivity(intent);
+    }
+
+    public void onIbProfile_Clicked(View caller){
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/be/kuleuven/queazy/ProfileActivity.java
@@ -0,0 +1,31 @@
+package be.kuleuven.queazy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Button btnLogOut2;
+    private Button btnBackToMenuPage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        btnLogOut2 = (Button) findViewById(R.id.btnLogOut2);
+        btnBackToMenuPage = (Button) findViewById(R.id.btnBackToMenuPage2);
+    }
+
+    public void onBtnLogOut2_Clicked(View caller){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onBtnBackToMenuPage2_Clicked(View caller){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+}
