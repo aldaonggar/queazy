5f461f669736e50cb2e7f46282d4ecd1d0b4d0c5
HOME-PC\asus <javad.nematli@student.kuleuven.be>
2022-05-08 17:59:51 +0200

    queazyyyyyyyyyyy

--- app/src/main/java/be/kuleuven/queazy/QuizActivity.java
+++ app/src/main/java/be/kuleuven/queazy/QuizActivity.java
@@ -3,12 +3,16 @@ package be.kuleuven.queazy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
     }
+
 }
