43de2bdf40ee59fd7b0f8ec67c2e7bc7aa6b52ca
aldaonggar <nur.aldaonggar@student.kuleuven.be>
2022-05-14 21:56:55 +0200

    asfsaddrsdqwadkjolasdsdjhjasddgf

--- app/src/main/java/be/kuleuven/queazy/QuizActivity.java
+++ app/src/main/java/be/kuleuven/queazy/QuizActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,13 +19,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class QuizActivity extends AppCompatActivity {
-    private int quizID;
-    private int questionNr;
+    private int quizID, questionNr, points, counter;
     private RequestQueue requestQueue;
     private Button btnAnswer1, btnAnswer2, btnAnswer3, btnAnswer4;
-    private TextView txtQuestionDB, txtCorrectOrWrong;
+    private TextView txtQuestionDB, txtPointsAdded, txtTimer;
 
-    private int iscorrect;
+    private int[] iscorrect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +36,8 @@ public class QuizActivity extends AppCompatActivity {
         btnAnswer3 = (Button) findViewById(R.id.btnAnswer3);
         btnAnswer4 = (Button) findViewById(R.id.btnAnswer4);
         txtQuestionDB = (TextView) findViewById(R.id.txtQuestionDB);
-        txtCorrectOrWrong = (TextView) findViewById(R.id.txtCorrectOrWrong);
+        txtPointsAdded = (TextView) findViewById(R.id.txtPointsAdded);
+        txtTimer = (TextView) findViewById(R.id.txtTimer);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -50,13 +51,14 @@ public class QuizActivity extends AppCompatActivity {
 
         JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET, requestURL, null,
             response -> {
-
+                iscorrect = new int[5];
                 for(int i = 0; i < response.length(); i++) {
                     try {
                         JSONObject o = response.getJSONObject(i);
                         String question = o.getString("question");
                         String answer = o.getString("answer");
-                        iscorrect = o.getInt("iscorrect");
+                        iscorrect[i] = o.getInt("iscorrect");
+                        System.out.println(iscorrect);
                         txtQuestionDB.setText(question);
                         BtnSetText(i, answer);
                     } catch (JSONException e) {
@@ -67,44 +69,58 @@ public class QuizActivity extends AppCompatActivity {
             error -> Toast.makeText(QuizActivity.this, "Unable to communicate with the server", Toast.LENGTH_LONG).show()
         );
         requestQueue.add(submitRequest);
+
     }
 
 
     public void BtnSetText(int i, String ans) {
         if (i == 0) {
             btnAnswer1.setText(ans);
-            onBtnAnsClicked(btnAnswer1);
+            onBtnAnsClicked(btnAnswer1, i);
         }
         if (i == 1) {
             btnAnswer2.setText(ans);
-            onBtnAnsClicked(btnAnswer2);
+            onBtnAnsClicked(btnAnswer2, i);
         }
         if (i == 2) {
             btnAnswer3.setText(ans);
-            onBtnAnsClicked(btnAnswer3);
+            onBtnAnsClicked(btnAnswer3, i);
         }
         if (i == 3) {
             btnAnswer4.setText(ans);
-            onBtnAnsClicked(btnAnswer4);
+            onBtnAnsClicked(btnAnswer4, i);
         }
     }
 
-    public void onBtnAnsClicked(Button myButton) {
+    public void onBtnAnsClicked(Button myButton, int i) {
         myButton.setOnClickListener((view) -> {
-            Intent intent = new Intent(this, QuizActivity.class);
-
-            if(iscorrect == 1){
-                txtCorrectOrWrong.setText("CORRECT!");
-                txtCorrectOrWrong.setTextColor(0x00FF0A);
-                intent.putExtra("quizID", quizID);
-                intent.putExtra("questionNr", questionNr + 1);
-                startActivity(intent);}
-            else if(iscorrect == 0) {
-                txtCorrectOrWrong.setText("TRY AGAIN!");
-                txtCorrectOrWrong.setTextColor(0xFF0000);
+            if(iscorrect[i] == 1){
+                myButton.setBackgroundColor(0xFF2FFF00);
+                txtPointsAdded.setText("Points earned: +");
             }
-
+            else if(iscorrect[i] == 0) {
+                myButton.setBackgroundColor(0xFFFF0000);
+                txtPointsAdded.setText("Points earned: +0");
+            }
+            moveToNext();
         });
+    }
+
+    public void moveToNext() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (questionNr < 5) {
+            Intent intent = new Intent(this, QuizActivity.class);
+            intent.putExtra("quizID", quizID);
+            intent.putExtra("questionNr", questionNr + 1);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }
     }
 
 }
